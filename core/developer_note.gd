5956d312fd03625e5f540500bef32ae982429215
Dmitry <KedroBoss@users.noreply.github.com>
2026-07-06 17:32:12 +0900

    fix: fixed the "Node not found: "%*" (relative to "*")." messages

--- core/developer_note.gd
+++ core/developer_note.gd
@@ -4,10 +4,15 @@ extends Sprite3D
 @export_multiline var note: String = "":
 	set(v):
 		note = v
-		if %Label: %Label.text = v
+		if is_inside_tree() and  %Label: %Label.text = v
 
 @export var text_color: Color:
 	set(v):
 		text_color = v
-		if %Label:
+		if is_inside_tree() and %Label:
 			%Label.label_settings.font_color = v
+
+func _ready() -> void:
+	if %Label:
+		%Label.text = note
+		%Label.label_settings.font_color = text_color
