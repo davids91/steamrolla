d110130201b6ed9a00061ea6019663f533edc2a0
Dmitry <KedroBoss@users.noreply.github.com>
2026-07-10 00:54:29 +0900

    feat: added asphalt bomb script and scene

---
+++ core/asphalt_bomb/asphalt_bomb.gd
@@ -0,0 +1,28 @@
+extends Node3D
+
+@export_category("Bomb parameters")
+@export var to_explode_time: float = 1.0
+@export var exlode_radius: float = 10.0
+@export var amount_to_add_asphalt: float = 20.0
+
+@export var to_explode := false
+
+@export var explosion_vfx_scene: PackedScene
+
+signal asphalt_bomb_exploded(blast_pos: Vector3, explode_radius: float, amount_to_add_asphalt: float)
+
+
+func _on_explode():
+	get_tree().create_timer(to_explode_time).timeout
+	asphalt_bomb_exploded.emit(global_position, exlode_radius, amount_to_add_asphalt)
+	if explosion_vfx_scene:
+		var vfx: Node3D = explosion_vfx_scene.instantiate()
+		get_tree().current_scene.add_child(vfx)
+		vfx.global_position = global_position
+	queue_free()
+	
+func _process(_delta: float) -> void:
+	if to_explode:
+		_on_explode()
+		to_explode = false
+	
