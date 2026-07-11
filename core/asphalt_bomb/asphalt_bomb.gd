e71e2230be9d8ec3410236072f45a46f8944c5cb
Dmitry <KedroBoss@users.noreply.github.com>
2026-07-11 15:59:58 +0900

    feat: added activation on BombActivator group entered

--- core/asphalt_bomb/asphalt_bomb.gd
+++ core/asphalt_bomb/asphalt_bomb.gd
@@ -1,5 +1,7 @@
 extends Node3D
 
+const ACTIVATE_GROUP = "BombActivator"
+
 @export_category("Bomb parameters")
 @export var to_explode_time: float = 1.0
 @export var exlode_radius: float = 10.0
@@ -12,17 +14,13 @@ extends Node3D
 signal asphalt_bomb_exploded(blast_pos: Vector3, explode_radius: float, amount_to_add_asphalt: float)
 
 
-func _on_explode():
-	get_tree().create_timer(to_explode_time).timeout
-	asphalt_bomb_exploded.emit(global_position, exlode_radius, amount_to_add_asphalt)
-	if explosion_vfx_scene:
-		var vfx: Node3D = explosion_vfx_scene.instantiate()
-		get_tree().current_scene.add_child(vfx)
-		vfx.global_position = global_position
-	queue_free()
-	
-func _process(_delta: float) -> void:
-	if to_explode:
-		_on_explode()
-		to_explode = false
-	
+
+func _on_area_3d_area_entered(body: Node3D) -> void:
+	if body.is_in_group(ACTIVATE_GROUP):
+		await get_tree().create_timer(to_explode_time).timeout
+		asphalt_bomb_exploded.emit(global_position, exlode_radius, amount_to_add_asphalt)
+		if explosion_vfx_scene:
+			var vfx: Node3D = explosion_vfx_scene.instantiate()
+			get_tree().current_scene.add_child(vfx)
+			vfx.global_position = global_position
+		queue_free()
