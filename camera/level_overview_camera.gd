b403b7d6a42f1e7e314f5650cc2eee23c10c43bd
ignipac <moshah-ignipac@outlook.com>
2026-07-17 13:02:55 +0100

    chore: clean up of the orbit camera script

--- camera/level_overview_camera.gd
+++ camera/level_overview_camera.gd
@@ -1,12 +1,10 @@
 class_name OrbitCamera3D extends Node3D
 
-@export var distance: float = 5.0
-
-@export var panning_speed: float = 0.01
 @export var orbit_speed: float = 0.005
 @export var zoom_speed: float = 0.5
+@export var panning_speed: float = 0.01
 
-@onready var camera_3d: Camera3D = %Camera3D
+@onready var _camera_3d: Camera3D = %Camera3D
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion and Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
@@ -15,13 +13,14 @@ func _unhandled_input(event: InputEvent) -> void:
 		
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
-			camera_3d.position.z += zoom_speed
+			_camera_3d.position.z += zoom_speed
 		elif event.button_index == MOUSE_BUTTON_WHEEL_UP:
-			camera_3d.position.z -= zoom_speed
+			_camera_3d.position.z -= zoom_speed
 			
 	if event is InputEventMouseMotion and Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT):
 		var offset := Vector3(-event.relative.x * panning_speed, event.relative.y * panning_speed, 0)
-		translate_object_local(offset)
+		var local_offset_transform = Transform3D(Basis.IDENTITY, offset)
+		set_transform(transform * local_offset_transform)
 		
 func _rotate_camera_by(offset: Vector2) -> void:
 	rotation.y -= offset.x
