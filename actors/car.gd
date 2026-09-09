8de955c8c24a617b8db1f3c78df2c1b39ede1ce3
Christer Kaitila <breakdance@mcfunkypants.com>
2026-09-09 16:38:38 -0700

    cars emit smoke + sound while being crushed

--- actors/car.gd
+++ actors/car.gd
@@ -10,8 +10,19 @@ func _ready() -> void:
 
 func _process(delta: float) -> void:
 	var roller_node: Node3D = get_node_or_null(crushes_me) as Node3D
+	var smoke: Node3D = get_node_or_null("smoke-particles") as Node3D
+	var crushSound: Node3D = get_node_or_null("CarCrush") as Node3D
 	if roller_node:
 		var dist: float = roller_node.global_position.distance_to(global_position)
-		print("car sees a roller! dist="+str(dist))
+		# print("car sees a roller! dist="+str(dist))
 		if dist <= crush_distance:
 			scale.y = lerp(scale.y, crush_scale, delta) # lol
+			if (smoke): smoke.emitting = true
+			if (crushSound): 
+				if not crushSound.playing: crushSound.play()
+				# crushSound.volume_db = linear_to_db(1-(dist/crush_distance))
+		else:
+			if (smoke): smoke.emitting = false
+			if (crushSound): 
+				if crushSound.playing: crushSound.stop()
+				# crushSound.volume_db = linear_to_db(0)
